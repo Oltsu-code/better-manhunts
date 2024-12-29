c80aa8bb7bf4fcc6abd0ff840fa78721cdd0e0c7
Oltsu-code <123672634+Oltsu-code@users.noreply.github.com>
2024-12-29 14:56:15 +0200

    a

---
+++ src/main/java/dev/oltsu/bettermanhunts/BetterManhunts.java
@@ -0,0 +1,16 @@
+package dev.oltsu.bettermanhunts;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterManhunts extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
